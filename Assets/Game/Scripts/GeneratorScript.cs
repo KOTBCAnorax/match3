19356e812f712a23ac6f51caaf322f5089a0d0db
KOTBCAnorax <47pohih@gmail.com>
2022-03-31 01:12:05 +0300

    Test commit

---
+++ Assets/Game/Scripts/GeneratorScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorScript : MonoBehaviour
+{
+    const int rows = 7;
+    const int cols = 6;
+    const int topRow = rows - 1;
+
+    GameObject[,] field;
+    GameObject[,] tokens;
+
+    [SerializeField] GameObject tilePrefab;
+    [SerializeField] GameObject tokenPrefab;
+    [SerializeField] GameObject fieldObject;
+    [SerializeField] GameObject tokensContainerObject;
+
+    [ContextMenu("GenerateField")]
+    [ContextMenu("GenerateTokens")]
+
+    void Start()
+    {
+         field = new GameObject[cols, rows];
+         tokens = new GameObject[cols, rows];
+         
+         GenerateField();
+         GenerateTokens();
+    }
+
+    void GenerateField()
+    {
+        for (int i = 0; i < cols; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                Vector3 pos = new Vector3(i, j, 0);
+                field[i,j] = Instantiate(tilePrefab, pos, Quaternion.identity);
+                field[i,j].transform.parent = fieldObject.transform;
+            }
+        }
+    }
+
+    void GenerateTokens()
+    {
+        for (int i = 0; i < cols; i++)
+        {
+            Vector3 pos = new Vector3(i, topRow, 0);
+            tokens[i, topRow] = Instantiate(tokenPrefab, pos, Quaternion.identity);
+            tokens[i, topRow].transform.parent = tokensContainerObject.transform;
+        }
+    }
+}
