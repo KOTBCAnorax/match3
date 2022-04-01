ed31860d6a6addef19578d7f9c2aeeca848c0107
KOTBCAnorax <47pohih@gmail.com>
2022-04-01 17:31:19 +0300

    Renamed variables rows and cols, added access modifiers where necessary, added function TopRowId()

--- Assets/Game/Scripts/GeneratorScript.cs
+++ Assets/Game/Scripts/GeneratorScript.cs
@@ -4,58 +4,60 @@ using UnityEngine;
 
 public class GeneratorScript : MonoBehaviour
 {
-    const int rows = 7;
-    const int cols = 6;
-    const int topRow = rows - 1;
+    [SerializeField] private int ROWS_COUNT = 7;
+    [SerializeField] private int COLUMNS_COUNT = 6;
 
-    [SerializeField] GameObject tilePrefab;
-    [SerializeField] GameObject tokenPrefab;
-    [SerializeField] GameObject fieldObject;
-    [SerializeField] GameObject tokensContainerObject;
+    [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private GameObject tokenPrefab;
+    [SerializeField] private GameObject fieldObject;
+    [SerializeField] private GameObject tokensContainerObject;
 
-    void Start()
+    private void Start()
     {
         GenerateField();
         GenerateTokens();
     }
 
     [ContextMenu("GenerateField")]
-    void GenerateField()
+    private void GenerateField()
     {
         if (HasChildren(fieldObject.transform))
         {
             return;
         }
 
-        for (int i = 0; i < cols; i++)
+        for (int i = 0; i < COLUMNS_COUNT; i++)
         {
-            for (int j = 0; j < rows; j++)
+            for (int j = 0; j < ROWS_COUNT; j++)
             {
                 Vector3 pos = new Vector3(i, j, 0);
-                GameObject newTile = Instantiate(tilePrefab, pos, Quaternion.identity);
-                newTile.transform.parent = fieldObject.transform;
+                Instantiate(tilePrefab, pos, Quaternion.identity, fieldObject.transform);
             }
         }
     }
 
     [ContextMenu("GenerateTokens")]
-    void GenerateTokens()
+    private void GenerateTokens()
     {
         if (HasChildren(tokensContainerObject.transform))
         {
             return;
         }
 
-        for (int i = 0; i < cols; i++)
+        for (int i = 0; i < COLUMNS_COUNT; i++)
         {
-            Vector3 pos = new Vector3(i, topRow, 0);
-            GameObject newToken = Instantiate(tokenPrefab, pos, Quaternion.identity);
-            newToken.transform.parent = tokensContainerObject.transform;
+            Vector3 pos = new Vector3(i, TopRowId(), 0);
+            Instantiate(tokenPrefab, pos, Quaternion.identity, tokensContainerObject.transform);
         }
     }
 
-    bool HasChildren(Transform obj)
+    private bool HasChildren(Transform obj)
     {
         return obj.childCount > 0;
+    }
+
+    private int TopRowId()
+    {
+        return ROWS_COUNT - 1;
     }
 }
